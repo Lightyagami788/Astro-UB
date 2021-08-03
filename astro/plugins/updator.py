b75235c393ee5ec705526dfd31a315dec4f4ec31
(◍•ᴗ•◍) <79841865+loverboyXD@users.noreply.github.com>
2021-08-03 13:58:54 +0530

    Update updator.py

--- astro/plugins/updator.py
+++ astro/plugins/updator.py
@@ -148,7 +148,7 @@ async def upstream(ups):
             repo.__del__()
             return
         await ups.edit(
-            "`Userbot dyno build in progress, please wait for it to complete.`"
+            "**Astro🌌Started to update....Just want till New deploy text in delivered in Your Private GROUP."
         )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
